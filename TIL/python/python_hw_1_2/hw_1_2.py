624400fbed00ddaa4e09269b723e21caab456a34
Tae Yeong Yu <88639688+ramge132@users.noreply.github.com>
2024-07-15 23:46:46 +0900

    add python set 2

--- TIL/python/python_hw_1_2/hw_1_2.py
+++ TIL/python/python_hw_1_2/hw_1_2.py
@@ -1,3 +1,14 @@
+""" 
+- password는 제공된 스켈레톤 코드를 참조한다.
+- 총 5개의 글자를 password 문장에서 찾아서 각각의 변수에 할당하여야 한다.
+- first_char 변수에 할당할 글자는 password 문자열의 28번째부터 35번째까지에 작성된 글자이다.
+- second_word 변수에 할당할 단어는 password 문자열의 113번째부터 총 5글자이다.
+- third_word 변수에 할당할 단어는 password 문자열의 66번째부터 68번째까지 작성된 글자를 뒤집어서 출력한다.
+- fourth_word 변수에 할당할 단어는 password 문자열의 322번째부터 총 4글자를 뒤집어서 출력한다.
+- fifth_word 변수에 할당할 단어는 password 문자열의 365번째부터 작성된 python이다.
+- 각 변수에 할당한 문자열을 f-string을 활용하여 출력하시오.
+ """
+
 password = "In the bustling city, where life is a constant race against time, uoy often find yourself wondering if there's a shortcut to success. The vibrant lights of the cityscape illuminate the night, casting shadows on the short-lived dreams of those who seek fortune. As you navigate through the crowded streets, you realize the deen for guidance, like a compass pointing python. You need direction in this chaotic journey called life."
 
 # 아래에 코드를 작성하시오.
@@ -8,5 +19,3 @@ forth_word = password[325:321:-1]
 fifth_word = password[365:371]
 
 print(f'{first_char} {second_word} {third_word} {forth_word} "{fifth_word}".')
-#print(f'{forth_word}')
-#test
--- TIL/python/python_ws_1_1/ws_1_1.py
+++ TIL/python/python_ws_1_1/ws_1_1.py
@@ -1,1 +1,10 @@
-# 아래에 코드를 작성하시오.
+""" 
+- username 변수에 'user123' 문자열을 할당한다.
+- age 변수에 20 정수를 할당한다.
+- is_active 변수에 True 불린 값을 할당한다.
+- 각 변수에 담긴 값을 출력한다.
+- f-string을 활용하여 'Username: user123, Age: 20, Active: True' 문자열을 출력한다.
+- 단, username, age, is_active 변수를 사용하여야 한다.
+- age 변수를 5만큼 증가시킨 후, 새로운 값을 출력한다.
+ """
+
