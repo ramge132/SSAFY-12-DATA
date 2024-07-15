015f6c838876196ea24531a8f63ca078b60bb72d
유태영 대전 7반 <yty96@naver.com>
2024-07-15 14:41:37 +0900

    .git deleted

---
+++ TIL/python/python_hw_1_2/hw_1_2.py
@@ -0,0 +1,11 @@
+password = "In the bustling city, where life is a constant race against time, uoy often find yourself wondering if there's a shortcut to success. The vibrant lights of the cityscape illuminate the night, casting shadows on the short-lived dreams of those who seek fortune. As you navigate through the crowded streets, you realize the deen for guidance, like a compass pointing python. You need direction in this chaotic journey called life."
+
+# 아래에 코드를 작성하시오.
+first_char = password[28:35]
+second_word = password[113:118]
+third_word = password[68:65:-1]
+forth_word = password[325:321:-1]
+fifth_word = password[365:371]
+
+print(f'{first_char} {second_word} {third_word} {forth_word} "{fifth_word}".')
+#print(f'{forth_word}')
